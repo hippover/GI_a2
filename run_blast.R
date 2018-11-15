075c9123b1bf2a6a2f9333eb163f97a274c35e04
degabriels <degabriels@gmail.com>
2018-11-15 13:49:14 +0000

    Add files via upload

--- run_blast.R
+++ run_blast.R
@@ -17,8 +17,8 @@ bl_dr<-'/local/data/public/genome_informatics_2018/programs/ncbi-blast-2.5.0+/bi
 
 ##align the genome onto the databases
 ##Dsim
-out_sim_n<-system(paste0(bl_dr,'blastn -db dsim_db -query ',Dmel_n,' -out out_sim_n -outfmt 6'),intern=T)
-print("dsim n alignment done")
+#out_sim_n<-system(paste0(bl_dr,'blastn -db dsim_db -query ',Dmel_n,' -out out_sim_n -outfmt 6'),intern=T)
+#print("dsim n alignment done")
 #out_sim_p<-system(paste0(bl_dr,'tblastn -db dsim_db -query ',Dmel_p,' -out out_sim_p -outfmt 6'),intern=T)
 #print("dsim p alignment done")
 
@@ -43,32 +43,55 @@ print("dsim n alignment done")
 ##read outputs
 require(seqinr)
 names<-c('sim','moj','ere','sec')
+mel_genes<-read.fasta(Dmel_n)#length=30828
+mel_prot<-read.fasta(Dmel_p)#length=30493
+genome_sec<-read.fasta(Dsec)
+genome_sim<-read.fasta(Dsim)
+genome_ere<-read.fasta(Dere)
+genome_moj<-read.fasta(Dmoj)
+colnames<-c('qseqid', 'sseqid', 'pident', 'length', 'mismatch', 'gapopen', 'qstart', 'qend', 'sstart', 'send', 'eval', 'bitscore')
+out_sec_n<-read.csv('out_sec_n',col.names=colnames,as.is=c(1,2),sep='\t',header=F)
+out_sec_p<-read.csv('out_sec_p',col.names=colnames,as.is=c(1,2),sep='\t',header=F)
+out_moj_n<-read.csv('out_moj_n',col.names=colnames,as.is=c(1,2),sep='\t',header=F)
+out_moj_p<-read.csv('out_moj_p',col.names=colnames,as.is=c(1,2),sep='\t',header=F)
+out_ere_n<-read.csv('out_ere_n',col.names=colnames,as.is=c(1,2),sep='\t',header=F)
+out_ere_p<-read.csv('out_ere_p',col.names=colnames,as.is=c(1,2),sep='\t',header=F)
+out_sim_n<-read.csv('out_sim_n',col.names=colnames,as.is=c(1,2),sep='\t',header=F)
+out_sim_p<-read.csv('out_sim_p',col.names=colnames,as.is=c(1,2),sep='\t',header=F)
 
-find_annotation_blast<-function(genome_file,blast_file){
+find_annotation_blast<-function(genome,out_complete){
   ret<-c()
-  colnames<-c('qseqid', 'sseqid', 'pident', 'length', 'mismatch', 'gapopen', 'qstart', 'qend', 'sstart', 'send', 'eval', 'bitscore')
-  out<-read.csv(blast_file,col.names=colnames,as.is=c(1,2),sep='\t',header=F)
-  genome<-read.fasta(genome_file)
+  out<-unique(out_complete[,c(2,3,9,10)])
   annotations <- vector(mode="list", length=length(genome))
   names(annotations)<-names(genome)
+  sum<-rep(0,length(genome))
   for(i in 1:length(genome)){
-    annotations[[i]]<-rep(0,length(genome[[i]]))
+    annotations[[i]]<-rep(F,length(genome[[i]]))
   }
-  for (l in 1:length(out)){
-    if(out[l,2] %in% names(annotations) & out[l,3]){
-      i<-which(names(annotations)==out[l,2])
-      I<-c(1:length(annotations[[i]]))
-      annotations[[i]][which(I>=out[l,9] & I<=out[l,10])]<-1
+  for (l in 1:nrow(out)){
+    if(out[l,1] %in% names(annotations) & out[l,2]>80){
+      i<-which(names(annotations)==out[l,1])
+      annotations[[i]][seq(min(out[l,3],out[l,4]),max(out[l,3],out[l,4]),1)]<-T
     }
   }
+  for(i in 1:length(genome)){
+    sum[i]<-sum(annotations[[i]])
+  }
+  ret$percent<-mean(out[which(out[,3]>80),3])
   ret$annotations<-annotations
-  ret$genes<-unique(out[,1])
+  ret$genes<-unique(out[which(out[,3]>80),1])
+  ret$sum<-sum
   return(ret)
 }
-annotations_sim_n<-find_annotation_blast(Dsim,'out_sim_n')
 
-percent<-vector(mode="list",length=length(names(sim_genome_file)))
-names(percent)<-names(annotations_sim_n)
-for (i in 1:length(annotations_sim_n)){
-  percent[[i]]<-mean(annotations_sim_n[[i]])
-}
+r_sim_n<-find_annotation_blast(genome_sim,out_sim_n)
+r_moj_n<-find_annotation_blast(genome_moj,out_moj_n)
+r_ere_n<-find_annotation_blast(genome_ere,out_ere_n)
+r_sec_n<-find_annotation_blast(genome_sec,out_sec_n)
+
+r_sim_p<-find_annotation_blast(genome_sim,out_sim_p)
+r_moj_p<-find_annotation_blast(genome_moj,out_moj_p)
+r_ere_p<-find_annotation_blast(genome_ere,out_ere_p)
+r_sec_p<-find_annotation_blast(genome_sec,out_sec_p)
+
+
